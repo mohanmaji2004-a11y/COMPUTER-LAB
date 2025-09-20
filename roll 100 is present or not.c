d8b52ab8f8747e142476ab0eb56d7ea8e3c39432
mohanmaji2004-a11y <mohanmaji2004@gmail.com>
2025-09-20 15:47:59 +0530

    Add files via upload

---
+++ roll 100 is present or not.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int roll = 100;
+
+    if (roll == 100) {
+        printf("Roll number 100 is present.\n");
+    } else {
+        printf("Roll number 100 is not present.\n");
+    }
+
+    return 0;
+}
